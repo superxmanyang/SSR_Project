8e73fe079c8147176f1c998b6b596cb4c67a9839
superxmanyang <460701143@qq.com>
2019-09-08 16:42:53 +0800

    控制列表展开

--- components/air/flightsItem.vue
+++ components/air/flightsItem.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flight-item">
-        <div>
+        <div @click="isShow=!isShow">
             <!-- 显示的机票信息 -->
             <el-row type="flex" align="middle" class="flight-info">
                 <el-col :span="6">
@@ -26,7 +26,7 @@
                 </el-col>
             </el-row>
         </div>
-        <div class="flight-recommend">
+        <div class="flight-recommend" v-show ="isShow">
             <!-- 隐藏的座位信息列表 -->
             <el-row type="flex"  justify="space-between" align="middle">
                 <el-col :span="4">低价推荐</el-col>
@@ -59,6 +59,15 @@
 
 <script>
 export default {
+   
+    data(){
+
+        return{
+            isShow:false
+        }
+    },
+
+  
 
     props: {
 
