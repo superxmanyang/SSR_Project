00a529ca2543c8f1eac078656ea95468f2b6c772
superxmanyang <460701143@qq.com>
2019-09-14 09:38:36 +0800

    绑定表单数据

--- components/air/orderForm.vue
+++ components/air/orderForm.vue
@@ -45,20 +45,20 @@
       <h2>联系人</h2>
       <div class="contact">
         <el-form label-width="60px">
-          <el-form-item label="姓名">
-            <el-input></el-input>
+          <el-form-item label="姓名" >
+            <el-input v-model="contactName"></el-input>
           </el-form-item>
 
-          <el-form-item label="手机">
-            <el-input placeholder="请输入内容">
+          <el-form-item label="手机"  >
+            <el-input placeholder="请输入内容" v-model="contactPhone">
               <template slot="append">
                 <el-button @click="handleSendCaptcha">发送验证码</el-button>
               </template>
             </el-input>
           </el-form-item>
 
-          <el-form-item label="验证码">
-            <el-input></el-input>
+          <el-form-item label="验证码"  >
+            <el-input v-model="captcha"></el-input>
           </el-form-item>
         </el-form>
         <el-button type="warning" class="submit" @click="handleSubmit">提交订单</el-button>
@@ -80,6 +80,22 @@ export default {
 
       // 保险数据id的集合
       insurances: [],
+
+
+      //后台需要的数据
+       contactName: "",    // 联系人
+       contactPhone: "",   // 联系电话
+       captcha: "",        // 验证码
+       invoice: false,     // 发票字段，默认false
+       seat_xid: "",       // 座位id，来自于url的参数
+       air: "" ,           // 航班的id,来自于url的id    
+
+
+
+
+
+
+
        // 机票的数据
       infoData:[],
 
@@ -139,16 +155,54 @@ export default {
  }
    
 
-     console.log( this.insurances);
+    //  console.log( this.insurances);
     },
   
   
-    // 发送手机验证码
-    handleSendCaptcha(){},
+    // 发送手机验证码 复制注册registerForm表单的功能
+    handleSendCaptcha(){
+
+   // 判断如果手机号码是空，不请求
+     if(!this.contactPhone){
+         this.$message.error("请输入手机号码");
+         return;
+     }
+     
+     
+     //发送验证码
+     this.$axios({
+       url:"/captchas",
+       method:"POST",
+       data:{
+         tel:this.contactPhone //手机号码
+       }
+     }).then(res=>{
+      //  console.log(res);
+       // 解构出code属性
+      const {code} = res.data;
+
+      // 文档地址：https://element.eleme.cn/#/zh-CN/component/message-box#xiao-xi-ti-shi
+     this.$alert(`模拟手机验证码是：${code}`, "提示");
+     })    
+
+
+
+    },
 
     // 提交订单
     handleSubmit() {
-      console.log(this.users);
+      // console.log(this.users);
+      const data = {
+                users: this.users,
+                insurances: this.insurances,
+                contactName: this.contactName,
+                contactPhone: this.contactPhone,
+                invoice: this.invoice,
+                captcha: this.captcha,
+                seat_xid: this.$route.query.seat_xid,
+                air: this.$route.query.id
+            }
+            console.log(data)
     }
   }
 };
